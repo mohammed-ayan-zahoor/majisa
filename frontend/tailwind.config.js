b5481ca5dfefc253df1d47e46c21e01e77915c42
mohammed-ayan-zahoor <mohammedayan76766@gmail.com>
2025-12-15 12:59:43 +0530

    feat: Initialize frontend structure with Tailwind CSS, custom fonts, and base styling.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -46,8 +46,8 @@ export default {
                 }
             },
             fontFamily: {
-                sans: ['Inter', 'sans-serif'],
-                serif: ['Playfair Display', 'serif'],
+                sans: ['Montserrat', 'sans-serif'],
+                serif: ['Mosseta', 'serif'],
             },
         },
     },
